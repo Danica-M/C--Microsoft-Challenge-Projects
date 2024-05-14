d2247a06d3a2b3927efff782c49444de1a917492
Danica-M <102434720+Danica-M@users.noreply.github.com>
2024-05-14 17:15:07 +1200

    first commit

---
+++ Challenge-1/grading-console-application1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+// initialize variables - graded assignments 
+int examAssignments = 5;
+
+//student scores arrays
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+int[] studentScores = new int[10];
+
+//initialise letter grade variable
+string currentStudentLetterGrade = "";
+
+//console application header
+Console.WriteLine("Student\t\tGrade\t\tLetterGrade\n");
+
+//looping through student name
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    //graded score counter
+    int gradedAssignment = 0;
+    //assigns specific student's score array to student score array
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+    else
+        continue;
+    //total score
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade = 0;
+    //loops through student array
+    foreach (int score in studentScores){
+        //graded assignment increments
+        gradedAssignment++;
+        //compares if the counter is less than or equal to the number of exam assignments
+        if (gradedAssignment <= examAssignments){
+            sumAssignmentScores+=score;
+        }
+        else{
+            //adds 10 percent of extra score
+            sumAssignmentScores+= score/10;
+        }
+        
+    }
+    //gets the average score
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
+
+    //gets the letter grade based on the percentage grade
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t\t{currentStudentLetterGrade}");
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
